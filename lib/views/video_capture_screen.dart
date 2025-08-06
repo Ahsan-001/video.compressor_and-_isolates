935a001925c1a45b02a04fd97fe37d2498410b06
Ahsan Saeed <ranaahsan5285@gmail.com>
2025-08-06 14:44:12 +0500

    capture video provider fixed

--- lib/views/video_capture_screen.dart
+++ lib/views/video_capture_screen.dart
@@ -52,20 +52,72 @@ class VideoCaptureScreen extends ConsumerWidget {
       body: Center(
         child:
             videoState.isLoading
-                ? const CircularProgressIndicator()
-                : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: recordVideo,
-                      child: const Text("Record Video (Camera)"),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: pickFromGallery,
-                      child: const Text("Pick Video (Gallery)"),
-                    ),
-                  ],
+                ? Center(
+                  child: const CircularProgressIndicator(color: Colors.white),
+                )
+                : Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          // foregroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minimumSize: const Size(200, 50),
+                        ),
+                        onPressed: recordVideo,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Record Video",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            // const Spacer(),
+                            SizedBox(width: 5),
+                            const Icon(
+                              Icons.camera_alt,
+                              size: 24,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          // foregroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minimumSize: const Size(200, 50),
+                        ),
+                        onPressed: pickFromGallery,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+
+                          children: [
+                            const Text(
+                              "From Gallery",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            // const Spacer(),
+                            SizedBox(width: 5),
+                            const Icon(
+                              Icons.photo_library,
+                              size: 24,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
       ),
     );
